5745e4a7f387b5a89166967dd6ba8e23d1021208
justmahmoud31 <justmahmoud3152003@gmail.com>
2024-11-25 16:54:03 +0200

    added : store configration

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import registerSlice from './Slices/Auth-Slice/RegisterReducer';
 const store = configureStore({
-    reducer :{
-
+    reducer: {
+        register: registerSlice
     }
 });
 export default store;
