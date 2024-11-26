9d3113f5fea0f8c39a3512efcfd8f9c0ca4909eb
amany_hesham <amanyhisham43@gmail.com>
2024-11-26 16:04:09 +0000

    added : reset password

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { Route, Routes } from "react-router-dom";
+import {  Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home";
 import Register from "./Pages/Auth/Register";
 import Login from "./Pages/Auth/Login";
 import ActivateAccount from "./Pages/Auth/ActivateAccount";
 import ForgetPassword from "./Pages/Auth/ForgetPassword";
+import ResetPassword from "./Pages/Auth/ResetPassword";
+ 
 
 function App() {
   return (
-    <>
+       
+
       <Routes>
         {/* Main Routes */}
         <Route path="/" element={<Home />} />
@@ -17,8 +20,11 @@ function App() {
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/forget-password" element={<ForgetPassword />} />
         <Route path="/auth/activate-account" element={<ActivateAccount />} />
+        <Route path="/auth/reset-password" element={<ResetPassword/>} />
+
       </Routes>
-    </>
+       
+
   );
 }
 
---
+++ src/Pages/Auth/ResetPassword.jsx
@@ -0,0 +1,54 @@
+ import Navbar from "../../Components/Navbar";
+import { Link } from "react-router-dom";
+import MainButton from "../../Components/Button";
+import Input from "../../Components/Input";
+import ResetPasswordImage from "../../assets/AuthAssets/ResetPassword.png";
+
+function ResetPassword() {
+  return (
+    <>
+      <Navbar currentTab={"Auth"} />
+      <div className="grid grid-cols-1 lg:grid-cols-2 h-screen">
+        { }
+        <div className="flex justify-center items-center bg-white p-6">
+          <div className="w-full max-w-md">
+            {/* this part of title page(ResetPassword)^^ */}
+            <div className="mb-8">
+              <h2 className="font-bold text-3xl">Set a new password</h2>
+              <p className="text-gray-600 mt-3">
+                Create a new password. Ensure it differs from previous ones for security.
+              </p>
+            </div>
+
+            {/* Updata password in this form that send DB*/}
+            <form className="space-y-6">
+              {/* FirstLabel*/}
+              <Input LabelText="Update Password" type="password" />
+              {/* SecondLabel  confirm*/}
+              <Input LabelText="Confirm Password" type="password" />
+              {/*  Button to updataPassword  */}
+              <div className="flex justify-center">
+                <MainButton
+                  className="bg-[#DB4444] text-white rounded-md py-3 px-6"
+                  ButtonText={"Update Password"}
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+
+        {/* image resetPassword */}
+        <div className="flex justify-center items-center p-6">
+          <img
+            src={ResetPasswordImage}
+            alt="Reset Password Illustration"
+            className="object-contain max-w-full rounded-lg"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ResetPassword;
+  
